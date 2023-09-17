f7332fecd45f51eb063154c30ae30eef390eafee
kys0417 <yungsang1111@gmail.com>
2023-09-17 22:12:38 +0900

    Feat: Add 공유하기, 반응형 제외 구현

--- 과제/10000-hours-rule/src/App.js
+++ 과제/10000-hours-rule/src/App.js
@@ -1,21 +1,34 @@
-import { React } from "styled-reset";
-import "./App.css"
+import { Reset } from "styled-reset";
+import { useState } from "react";
+import "./App.css";
 import Header from "./components/Header/Header";
 import Intro from "./components/Intro/Intro";
 import Form from "./components/Form/Form";
 import Result from "./components/Result/Result";
 import Modal from "./components/Modal/Modal";
 import Footer from "./components/Footer/Footer";
+import Loading from "./components/Loading/Loading";
 
 function App() {
+  const [showModal, setShowModal] = useState(false);
+  const [showResult, setShowResult] = useState(false);
+  const [showLoading, setShowLoading] = useState(false);
+  const [inputGoal, setInputGoal] = useState("");
+  const [inputTime, setInputTime] = useState("");
+
   return (
     <div className="app">
+      <Reset></Reset>
       <Header></Header>
       <Intro></Intro>
-      <Form></Form>
-      <Result></Result>
+      <Form setInputGoal={setInputGoal} setInputTime={setInputTime} setShowResult={setShowResult} setShowLoading={setShowLoading}></Form>
+      {showLoading ? (
+        <Loading></Loading>
+      ) : (
+        showResult && <Result inputGoal={inputGoal} inputTime={inputTime} setShowModal={setShowModal}></Result>
+      )}
       <Footer></Footer>
-      <Modal></Modal>
+      {showModal && <Modal setShowModal={setShowModal}></Modal>}
     </div>
   );
 }
--- 과제/10000-hours-rule/src/components/Modal/Modal.jsx
+++ 과제/10000-hours-rule/src/components/Modal/Modal.jsx
@@ -1,10 +1,24 @@
-import React from 'react';
+import React from "react";
+import "./Modal.css";
 
-function Modal() {
+function Modal({setShowModal}) {
+  const offModal = () => {
+    setShowModal(false);
+  }
   return (
-    <div>
-      
-    </div>
+    <section className="modal">
+      <div className="field_modal">
+        <div className="cheer">
+          <h1>화이팅!!♥♥♥</h1>
+          <h3>당신의 꿈을 응원합니다!</h3>
+        </div>
+        <img className="img_lion" src="img/lion.svg" alt="" />
+        <div className="field_button_close">
+          <button className="button_close" onClick={offModal}>종료하고 진짜 훈련하러 가기 GO!GO!</button>
+          <p>(그냥 닫기 버튼입니다.)</p>
+        </div>
+      </div>
+    </section>
   );
 }
 
